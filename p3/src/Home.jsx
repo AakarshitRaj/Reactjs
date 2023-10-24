57871be1d37a74ffdd35995e6960f9ba06727aa4
Aakarshit <rajaakarshit7@gmail.com>
2023-10-25 00:08:13 +0530

    p3 commit

---
+++ p3/src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+      This is Home Page
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Home
