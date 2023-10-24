694cfada452d8657e7b95d4039777380de3925f3
Aakarshit <rajaakarshit7@gmail.com>
2023-10-25 00:49:12 +0530

    p5 commit and implement header.js and short the code in home,about,contact section

--- p5/src/About.jsx
+++ p5/src/About.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import Header from './Header'
 const About = () => {
   return (
     <div>
-    
-      <header>
-      This is About Page
-        <nav>
-            <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/contact'>Contact</NavLink></li>
-            </ul>
-        </nav>
-      </header>
+    <Header/>
+    <section>
+        <h1>This is About Page</h1>
+      </section>
     </div>
   )
 }
--- p5/src/Home.jsx
+++ p5/src/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import Header from './Header'
 const Home = () => {
   return (
     <div>
-      <header>
-      This is Home Page
-        <nav>
-            <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/contact'>Contact</NavLink></li>
-            </ul>
-        </nav>
-      </header>
+      <Header/>
+      <section>
+        <h1>This is Home Page</h1>
+      </section>
     </div>
   )
 }
