f3d7d247a3ba0a45ca72eae2bc2298fc43531165
Aakarshit <rajaakarshit7@gmail.com>
2023-10-25 13:32:46 +0530

    nested introduce and stuck

---
+++ nested/src/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+const Contact = () => {
+  return (
+    <div>
+      <Header/>
+      <section>
+        <h1>This is Contact Page</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Contact
---
+++ nested/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <section>
+        <h1>This is Home Page</h1>
+      </section>
+    </div>
+  )
+}
+
+export default Home
